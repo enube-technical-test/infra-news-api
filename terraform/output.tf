b30c8e3c164357e4ae723a382b5a13f4328f5677
Juan Yustes <yustes2017@gmail.com>
2025-10-06 11:56:15 -0500

    fix: vpc error at defined, changed to the default aws vpc

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,15 +1,15 @@
-output "ecr_repo_url" {
-  value = aws_ecr_repository.repo.repository_url
+output "vpc_id" {
+  value = data.aws_vpc.default.id
+}
+
+output "subnet_ids" {
+  value = data.aws_subnets.default.ids
 }
 
 output "eks_cluster_name" {
   value = aws_eks_cluster.eks.name
 }
 
-output "kubeconfig_command" {
-  value = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.eks.name}"
-}
-
-output "subnet_ids" {
-  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+output "ecr_repo_url" {
+  value = aws_ecr_repository.repo.repository_url
 }
